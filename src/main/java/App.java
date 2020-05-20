58f933afd292108f55d1e501d9a35129dfb72eee
Jonathan Araujo <Jonathan_Araujo@gap.com>
2020-05-20 05:30:54 -0300

    Updating cache/database

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,7 +9,7 @@ import java.util.stream.Stream;
 
 public class App {
 
-    private static final String SEARCH_STRING = "registeel:12,altaria:12,azumarill:11,skarmory:8,toxicroak:8";
+    private static final String SEARCH_STRING = "azumarill:23,registeel:19,altaria:19,skarmory:13,hypno:10,umbreon:10\n";
 
     private static final String DEFAULT_LEAGUE = "Great";
     private static final String DEFAULT_NUMBER_OF_SHIELDS = "1";
